8a3407debbd0408979f53c1545c608e8c6220749
StefanoPetrilli <stefanop_1999@hotmail.it>
2023-01-24 09:58:19 +0200

    Added login view and controller (#10)

---
+++ src/controller/login/login_controller.cc
@@ -0,0 +1,22 @@
+//
+// Created by Stefano on 1/22/2023.
+//
+
+#include "login_controller.h"
+
+namespace login {
+void AddLoginController(crow::SimpleApp *app) {
+  crow::SimpleApp &aabrham = *app;
+
+  CROW_ROUTE(aabrham, "/login/")([]() {
+    crow::mustache::context ctx;
+    return crow::mustache::load_text("login/loginTemplate.html");
+  });
+
+  CROW_ROUTE(aabrham, "/loginDirective.js")([]() {
+    crow::mustache::context ctx;
+    return crow::mustache::load_text("login/loginDirective.js");
+  });
+
+}
+}
