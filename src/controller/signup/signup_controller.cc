14cd1250b89f7a0b24717ecfd496c869cf424453
StefanoPetrilli <stefanop_1999@hotmail.it>
2023-01-25 22:37:03 +0200

    Implemented api call for the signup (#13)

--- src/controller/signup/signup_controller.cc
+++ src/controller/signup/signup_controller.cc
@@ -5,7 +5,7 @@
 #include "signup_controller.h"
 
 namespace signup {
-void AddSignupController(crow::SimpleApp* app) {
+void AddSignupController(crow::SimpleApp *app) {
   crow::SimpleApp &aabrham = *app;
 
   CROW_ROUTE(aabrham, "/signup")([]() {
@@ -16,6 +16,15 @@ void AddSignupController(crow::SimpleApp* app) {
   CROW_ROUTE(aabrham, "/signupDirective.js")([]() {
     crow::mustache::context ctx;
     return crow::mustache::load_text("signup/signupDirective.js");
+  });
+
+  CROW_ROUTE(aabrham, "/signup/api/<string>/<string>")([](const std::string &username, const std::string &password) {
+
+    crow::json::wvalue x;
+    x["result"] = false;
+    x["error"] = "Placeholder error" + username + password;
+
+    return crow::response(std::move(x));
   });
 }
 }
