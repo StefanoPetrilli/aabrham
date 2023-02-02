ef5512f5f9dc08a8b515cf686a78a89e0f324554
StefanoPetrilli <stefanop_1999@hotmail.it>
2023-02-02 23:29:31 +0200

    Adds tests related to RedisConnection (#12)

---
+++ test/infrastructure/redis/redis_test.cc
@@ -0,0 +1,28 @@
+//
+// Created by Stefano on 2/2/2023.
+//
+#include "redis_test.h"
+
+namespace redis_connection_test {
+
+void RedisTest::SetUp() {
+  redis_connection = redis_connection::RedisConnection();
+  if (!redis_connection.IsConnected())
+    GTEST_SKIP_("Test skipped because it is impossible to connect to the redis database");
+}
+
+TEST_F(RedisTest, Expect_CorrectWrite) {
+  EXPECT_TRUE(redis_connection.HashSet("TestKey", "TestField", "TestValue"));
+}
+
+TEST_F(RedisTest, Expect_SuccesfullReadFromDatabase) {
+  auto result = redis_connection.HashGet("TestKey", "TestField").value();
+  auto expected = "TestValue";
+
+  EXPECT_EQ(expected, result);
+}
+
+TEST_F(RedisTest, Expect_SuccesfullDeleteFromDatabase) {
+  EXPECT_TRUE(redis_connection.KeyDelete("TestKey"));
+}
+}
