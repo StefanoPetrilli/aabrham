bcb67b881cff78c3ee3e018d052bd4be2742ff18
StefanoPetrilli <stefanop_1999@hotmail.it>
2023-02-16 09:59:57 +0200

    Moved docker build tool to appropriate folder (#26)

---
+++ build_tools_setup.sh
@@ -0,0 +1,2 @@
+#! /bin/sh
+docker build -t stefanopetrilli/aabrham-build-container:0.1 -f build_tools/DockerfileBuildEnvironment .
--- setup.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/sh
-docker build -t stefanopetrilli/aabrham-container:0.3 -f Dockerfile .
