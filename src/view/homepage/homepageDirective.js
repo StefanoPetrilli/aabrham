bfebc31b27336001f46552dfa082c1589e7a4ccb
StefanoPetrilli <stefanop_1999@hotmail.it>
2023-02-18 21:41:33 +0200

    Modified frontend to insert new item in the database (#28)

--- src/view/homepage/homepageDirective.js
+++ src/view/homepage/homepageDirective.js
@@ -1,11 +1,24 @@
 var aabrham = angular.module('aabrham', []);
 
 aabrham.controller('HomepageController', function HomepageController($scope, $http) {
-    $scope.signout = function(){
+    $scope.signout = function () {
         $http.get("/homepage/api/signout").then(function (result) {
             result.data.result ?
                 window.location.href = "/" :
                 $scope.failedLogin = true;
+        });
+    }
+
+    $scope.insertItem = function (itemName) {
+        let message = {
+            itemName: itemName
+        };
+
+        $http.post("/api/item/insert", JSON.stringify(message)).then(function (result) {
+            if (!result.data.result) {
+                $scope.errorMessage = result.data.error;
+                $scope.insertError = true;
+            }
         });
     }
 });
